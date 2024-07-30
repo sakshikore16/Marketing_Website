2d54f0e8929bc18e9aea0f8e280aa130b1ab411a
Sakshi Kore <142594806+sakshikore16@users.noreply.github.com>
2024-07-30 11:33:17 +0530

    Update Case.jsx

--- src/components/Case.jsx
+++ src/components/Case.jsx
@@ -19,33 +19,21 @@ const Cases = () => {
             <div className="w-full text-lg font-normal">
               For a local restaurant, we implemented a targeted PPC campaign that resulted in a 50% increase in website traffic and a 25% increase in sales.
             </div>
-            <div className="justify-start items-center gap-[15px] inline-flex">
-              <div className="text-lime text-xl font-normal leading-7">Learn more</div>
-              <img src="/services/agc.svg" alt="arrow" />
-            </div>
           </div>
           <div className="flex-col items-start gap-5 inline-flex px-2 md:px-6 py-6 md:py-2">
             <div className="w-full text-lg font-normal">
               For a B2B software company, we developed an SEO strategy that resulted in a first page ranking for key keywords and a 200% increase in organic traffic.
-            </div>
-            <div className="justify-start items-center gap-[15px] inline-flex">
-              <div className="text-lime text-xl font-normal leading-7">Learn more</div>
-              <img src="/services/agc.svg" alt="arrow" />
             </div>
           </div>
           <div className="flex-col items-start gap-5 inline-flex px-2 md:px-6 py-6 md:py-2">
             <div className="w-full text-lg font-normal">
               For a national retail chain, we created a social media marketing campaign that increased followers by 25% and generated a 20% increase in online sales.
             </div>
-            <div className="justify-start items-center gap-[15px] inline-flex">
-              <div className="text-lime text-xl font-normal leading-7">Learn more</div>
-              <img src="/services/agc.svg" alt="arrow" />
-            </div>
           </div>
         </div>
       </div>
 
-      <div className="relative flex flex-col items-center md:flex-row pt-6">
+      <div className="relative flex flex-col items-center md:flex-row pt-16">
         <Topic title="Results" description="The Results that We have delivered to our Clients" />
       </div>
 
